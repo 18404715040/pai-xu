27871afebd2ca9d66dddf94701785b342d7cb14d
20141105074 <20141105074qf@mac25.local>
2016-06-21 10:14:18 +0800

    shuzupaixu
    用swift语句编写一个对20个数字进行从大到小的排序，先定义数组20，再定义变量i和j并且给变量赋初始值，再定义一个中间变量t同上赋值，再运用
    for语句和if语句对这数组20种的数字进行排序，在if语句中使其每两个进行比较，通过中间变量进行将比较完的数字归位，将此比较在for语句中进行
    20次循环，输出比较结果

--- pai xu/main.swift
+++ pai xu/main.swift
@@ -8,5 +8,23 @@
 
 import Foundation
 
-print("Hello, World!")
+var a = [22,13,1,3,2,56,32,12,20,5,10,67,35,78,9,18,57,29,36,40,59]
+var i = 0
+var j = 0
+var t = 0
+for i = 0;i<a.count-1;i++
+{
+    for j=0;j<a.count-1;j++
+    {
+       if(a[j]<a[j+1])
+        {
+           t=a[j]
+           a[j]=a[j+1]
+           a[j+1]=t
+        }
+    }
+}
+print(a)
+
+
 
